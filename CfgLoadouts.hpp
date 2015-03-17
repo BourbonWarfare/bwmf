5ee32628ec473eab600e0af9804377515abbfe30
PabstMirror <elephantisaterriblemascot@gmail.com>
2015-03-16 21:44:24 -0500

    Adds basic civilian units

--- CfgLoadouts.hpp
+++ CfgLoadouts.hpp
@@ -1114,8 +1114,37 @@ class rhs_faction_vvs {
 		};
 
 	};
+    class CIV_F {
+        class C_man_1 {
+            uniform[] = {"U_C_Poloshirt_blue","U_C_Poloshirt_burgundy","U_C_Poloshirt_stripped","U_C_Poloshirt_tricolour","U_C_Poloshirt_salmon","U_C_Poloshirt_redwhite"};  /// randomized
+            vest[] = {}; /// randomized
+            headgear[] = {}; /// randomized
+            backpack[] = {}; /// randomized
+            backpackItems[] = {};
+            weapons[] = {}; /// randomized
+            launchers[] = {}; /// randomized
+            handguns[] = {}; /// randomized
+            magazines[] = {};
+            items[] = {"AGM_Bandage:3"};
+            linkedItems[] = {"ItemMap","ItemCompass","ItemWatch"};
+            attachments[] = {};
+        };
+        class C_journalist_F: C_man_1 {
+            uniform[] = {"U_C_Journalist"};  /// randomized
+            vest[] = {"V_Press_F"}; /// randomized
+            headgear[] = {"H_Cap_press"}; /// randomized
+        };
+        class CEO: C_man_1 {
+            uniform[] = {"U_Marshal"};  /// randomized
+            headgear[] = {"H_Hat_brown"};
+            items[] += {"CUP_item_Money:10"};
+        };
+        class C_man_hunter_1_F: C_man_1 {
+            uniform[] = {"U_C_HunterBody_grn"};  /// randomized
+            headgear[] = {"H_Booniehat_grn"};  /// randomized
+            weapons[] = {"CUP_srifle_LeeEnfield"};
+            magazines[] = {"CUP_10x_303_M:3"};
+            backpack[] = {"B_OutdoorPack_blk"};
+        };
+    };
 };
-
-
-
-
