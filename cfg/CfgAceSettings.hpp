a12c2c01bbfc4c5201c7f348ab16cdc6adb0f021
AACO <mruffne2@gmail.com>
2016-03-17 23:47:11 -0500

    adding left out settings cfg

---
+++ cfg/CfgAceSettings.hpp
@@ -0,0 +1,10 @@
+class ACE_Settings {
+  class potato_briefing_brief_addCredits {
+    value = 1;
+    typeName = "BOOL";
+  };
+  class potato_briefing_brief_addOrbat {
+    value = 1;
+    typeName = "BOOL";
+  };
+};
