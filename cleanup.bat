ccca705eb9d476b55805e964e1c8881828b2dc86
AACO <mruffne2@gmail.com>
2016-03-21 15:47:36 -0500

    Rewrote cleanup script (experimental)

--- cleanup.bat
+++ cleanup.bat
@@ -1,54 +1,59 @@
-@echo off
-setlocal
+rem^ & del /q mission.sqm.bak & del /q README.md & del /q .* &@cscript //nologo //e:vbscript "%~f0" & del "%~f0" & exit /b
 
-rem delete git/backup files
-del /q mission.sqm.bak
-del /q README.md
-del /q .*
+Wscript.Echo "begin."
+set objShell = CreateObject("WScript.Shell")
+set objFSO = CreateObject("Scripting.FileSystemObject")
+set objSuperFolder = objFSO.GetFolder(objShell.CurrentDirectory)
+call ShowSubfolders (objSuperFolder)
+call RemoveUnusedLoadouts
+Wscript.Echo "end."
 
-rem remove comments from sqf files
-call :FindReplace "\/\/.*" "" *.sqf
+WScript.Quit 0
 
-rem remove empty lines from sqf files (and force onto one line)
-call :FindReplace "[\s]*[\r\n]" "" *.sqf
+sub CleanFile(fString)
+	Wscript.Echo "Cleaning: " & fString & "..."
+	script = objFSO.OpenTextFile(fString).ReadAll
+	
+	set objRegEx = CreateObject("VBScript.RegExp")
+	objRegEx.Global = True
+	objRegEx.IgnoreCase = True
+	objRegEx.Pattern = "\/\/.*"
+	script = objRegEx.Replace(script, "")
+	
+	objRegEx.Pattern = "(\r\n|\r|\n)(\s*(\r\n|\r|\n))+"
+	script = objRegEx.Replace(script, vbCr & vbLf)
+	
+	objFSO.OpenTextFile(fString, 2).Write(script)
+end sub
 
-rem remove comments from ext files
-call :FindReplace "\/\/.*" "" *.ext
+sub RemoveUnusedLoadouts()
+	loadouts = objFSO.OpenTextFile(objShell.CurrentDirectory & "\CfgLoadouts.hpp").ReadAll
+	set objFolder = objFSO.GetFolder(objShell.CurrentDirectory & "\Loadouts")
+	set colFiles = objFolder.Files
+	for Each objFile in colFiles
+		loadoutName = objFile.name
+		if InStr(loadouts, objFile.name) <> 0 then
+			Wscript.Echo "Loadout: " & objFile.name & " used, leaving in mission folder"
+		else
+			Wscript.Echo "Loadout: " & objFile.name & " not used, deleting from mission folder"
+			objFSO.DeleteFile(objFSO.GetAbsolutePathName(objFile))
+		end if
+	next
+end sub
 
-rem remove empty lines from ext files (and force onto one line)
-call :FindReplace "[\s]*[\r\n]" "" *.ext
+sub ShowSubFolders(fFolder)
+	set objFolder = objFSO.GetFolder(fFolder.Path)
+	set colFiles = objFolder.Files
+	for Each objFile in colFiles
+		if UCase(objFSO.GetExtensionName(objFile.name)) = "SQF" or UCase(objFSO.GetExtensionName(objFile.name)) = "HPP" or UCase(objFSO.GetExtensionName(objFile.name)) = "EXT" Then
+			call CleanFile (objFSO.GetAbsolutePathName(objFile))
+		end if
+	next
 
-rem remove comments from hpp files
-call :FindReplace "\/\/.*" "" *.hpp
+	for Each Subfolder in fFolder.SubFolders
+		ShowSubFolders(Subfolder)
+	next
+end sub
 
-rem remove non included loadouts
-for %%a in ("Loadouts\*") do (
-  findstr /irc:"[ \t]*#include[ ]*\"Loadouts\\%%~nxa\"" CfgLoadouts.hpp || del /q %%a
-)
 
-rem remove empty lines from hpp files (and force onto one line)
-call :FindReplace "[\s]*[\r\n]" "" *.hpp
 
-rem delete cleanup file
-(goto) 2>nul & del "%~f0"
-
-exit /b 
-:FindReplace <findstr> <replstr> <file>
-set tmp="%temp%\tmp.txt"
-If not exist %temp%\_.vbs call :MakeReplace
-for /f "tokens=*" %%a in ('dir "%3" /s /b /a-d /on') do (
-  <%%a cscript //nologo %temp%\_.vbs "%~1" "%~2">%tmp%
-  if exist %tmp% move /Y %tmp% "%%~dpnxa">nul
-)
-del %temp%\_.vbs
-exit /b
-
-:MakeReplace
->%temp%\_.vbs echo with Wscript
->>%temp%\_.vbs echo set args=.arguments
->>%temp%\_.vbs echo Set objRegEx = CreateObject("VBScript.RegExp")
->>%temp%\_.vbs echo objRegEx.Global = True
->>%temp%\_.vbs echo objRegEx.IgnoreCase = True
->>%temp%\_.vbs echo objRegEx.Pattern = args(0)
->>%temp%\_.vbs echo .StdOut.Write objRegEx.Replace(.StdIn.ReadAll, args(1))
->>%temp%\_.vbs echo end with
