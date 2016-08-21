1a08a2489785715560a40c115719bc2979ed169f
AACO <mruffne2@gmail.com>
2016-08-20 19:49:49 -0500

    Update respawn to work with the new marker/radio systems.

--- cfg/CfgEventHandlers.hpp
+++ cfg/CfgEventHandlers.hpp
@@ -1,24 +1,23 @@
 //Extended Event Handlers:
-
 class Extended_InitPost_EventHandlers {
   class CAManBase {
-    class F_FixFriendlyFire { init = "if (local (_this select 0)) then {(_this select 0) addRating 100000;};";};
+    class BWMF_FixFriendlyFire { init = "if (local (_this select 0)) then {(_this select 0) addRating 100000;};";};
   };
   class Car {
-    class F_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
-    class F_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
   };
   class Tank {
-    class F_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
-    class F_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
   };
   class Helicopter {
-    class F_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
   };
   class Plane {
-    class F_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
   };
   class Ship_F {
-    class F_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
   };
 };
--- f/respawn/respawn_gui_code.sqf
+++ f/respawn/respawn_gui_code.sqf
@@ -207,8 +207,6 @@ fn_respawnMenuRespawnAction = {
   _control = ((findDisplay 26893) displayCtrl 26898);
   _groupIndex = lbCurSel _control;
   _groupName = (respawnMenuGroupNames select _groupIndex) select 0;
-  _sr = (respawnMenuGroupNames select _groupIndex) select 1;
-  _lr = (respawnMenuGroupNames select _groupIndex) select 2;
   if (_groupName == "") exitWith { hint "No group name selected"; };
 
   // Hand over control to the map dialog.
@@ -216,9 +214,7 @@ fn_respawnMenuRespawnAction = {
   createDialog "respawnMenuMapDialog";
 
   var1_faction = _faction;
-  var2_groupName = _groupName;
-  var3_sr = _sr;
-  var4_lr = _lr;
+  var2_groupIndex = _groupIndex;
 };
 
 fn_respawnMapLoaded = {
@@ -289,7 +285,8 @@ fn_respawnMap_keyUp = {
         if (!(_pos isEqualTo [0,0,0])) then { _position = _pos; };
       };
       hint "Group created on ground.";
-      [[var2_groupName, _position, var1_faction, selectedRespawnGroup, var3_sr, var4_lr], "F_fnc_RespawnWaveServer", false] call BIS_fnc_MP;
+
+      [var2_groupIndex, _position, var1_faction, selectedRespawnGroup] remoteExecCall ["F_fnc_RespawnWaveServer", 2, false];
       selectedRespawnGroup = [];
 
       // Close the Map
