a1d38c8bd0e6e5b0cc54279242975f37fed6a2bc
PabstMirror <pabstmirror@gmail.com>
2017-11-12 16:59:49 -0600

    x

--- CfgLoadouts.hpp
+++ CfgLoadouts.hpp
@@ -48,23 +48,22 @@ class CfgLoadouts {
 
   // West factions
   #define SIDE_WEST
-  #include "Loadouts\side_gear.hpp"
   class blu_f { // BluFor
     #include "Loadouts\us_m4_ucp.hpp"
   };
   #undef SIDE_WEST
 
+
   // Indy factions
   #define SIDE_INDY
-  #include "Loadouts\side_gear.hpp"
   class ind_f { // Indy
     #include "Loadouts\ukr_ak74_ttsko.hpp"
   };
   #undef SIDE_INDY
 
+
   // East Factions
   #define SIDE_EAST
-  #include "Loadouts\side_gear.hpp"
   class opf_f { // OpFor
     #include "Loadouts\ru_ak74_floral.hpp"
   };
@@ -74,9 +73,9 @@ class CfgLoadouts {
   };
   #undef SIDE_EAST
 
+
   // Civilians (mainly for RP missions)
   #define SIDE_CIV
-  #include "Loadouts\side_gear.hpp"
   #include "Loadouts\civilians.hpp" // Bare example of doing civilian loadouts
   #undef SIDE_CIV
 };
--- Loadouts/undef.hpp
+++ Loadouts/undef.hpp
@@ -1,3 +1,44 @@
+// UNDEF
+#ifdef KEY
+  #undef KEY
+#endif
+#ifdef CHEM_LIGHT
+  #undef CHEM_LIGHT
+#endif
+#ifdef UAV_BACKPACK
+  #undef UAV_BACKPACK
+#endif
+#ifdef UAV_TERMINAL
+  #undef UAV_TERMINAL
+#endif
+
+#ifdef SIDE_WEST
+  #define KEY "ACE_key_west"
+  #define CHEM_LIGHT "Chemlight_blue"
+  #define UAV_BACKPACK "B_UAV_01_backpack_F"
+  #define UAV_TERMINAL "B_uavterminal"
+#endif
+#ifdef SIDE_EAST
+  #define KEY "ACE_key_east"
+  #define CHEM_LIGHT "Chemlight_red"
+  #define UAV_BACKPACK "O_UAV_01_backpack_F"
+  #define UAV_TERMINAL "O_uavterminal"
+#endif
+#ifdef SIDE_INDY
+  #define KEY "ACE_key_indp"
+  #define CHEM_LIGHT "Chemlight_green"
+  #define UAV_BACKPACK "I_UAV_01_backpack_F"
+  #define UAV_TERMINAL "I_uavterminal"
+#endif
+#ifdef SIDE_CIV
+  #define KEY "ACE_key_civ"
+  #define CHEM_LIGHT "Chemlight_yellow"
+  #define UAV_BACKPACK "C_IDAP_UAV_01_backpack_F"
+  #define UAV_TERMINAL "C_uavterminal"
+#endif
+
+
+
 #ifdef CAMO_UNIFORM
   #undef CAMO_UNIFORM
 #endif
