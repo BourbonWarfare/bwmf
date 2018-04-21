64a16fe456a8ecf6e6479b70c0ee2daf0773dad0
PabstMirror <pabstmirror@gmail.com>
2018-04-21 15:33:39 -0500

    Allow thermal in blu LSV (ace_javelin locking req thermal)

--- cfg/CfgEventHandlers.hpp
+++ cfg/CfgEventHandlers.hpp
@@ -2,7 +2,7 @@
 class Extended_InitPost_EventHandlers {
   class Car {
     class BWMF_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
-    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; };
+    class BWMF_DisableThermals { init = "(_this select 0) disableTIEquipment true;"; exclude = "LSV_01_AT_base_F"; };
   };
   class Tank {
     class BWMF_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
