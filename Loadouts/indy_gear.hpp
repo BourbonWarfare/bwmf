b9924672907f9c38b6e1fa6db5bfb49291dab38a
Alablm <aidan197@hotmail.com>
2020-09-28 14:15:56 -0400

    FAC Smoke
    Gives FACs a variety of smoke colours to use

--- Loadouts/indy_gear.hpp
+++ Loadouts/indy_gear.hpp
@@ -4,4 +4,4 @@
 #define SIDE_CHEM_LIGHT "Chemlight_green:2"
 #define SIDE_UAV_BACKPACK "I_UAV_01_backpack_F"
 #define SIDE_UAV_TERMINAL "I_uavterminal"
-#define SIDE_FAC_GRENADES "SmokeShell:2","HandGrenade:2","1Rnd_SmokeOrange_Grenade_shell:10","1Rnd_SmokeGreen_Grenade_shell:5"
+#define SIDE_FAC_GRENADES "SmokeShell:2","HandGrenade:2","1Rnd_SmokeBlue_Grenade_shell:4","1Rnd_SmokeYellow_Grenade_shell:3","1Rnd_SmokeOrange_Grenade_shell:3","1Rnd_SmokeGreen_Grenade_shell:5"
