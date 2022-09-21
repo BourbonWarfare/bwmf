ab63beb5edc8f418c0fc3e00a74765ebd84ae4f5
PabstMirror <pabstmirror@gmail.com>
2022-09-21 00:41:19 -0500

    fix rhs tow

--- Loadouts/Blufor/us_hk416_mtp_apex.hpp
+++ Loadouts/Blufor/us_hk416_mtp_apex.hpp
@@ -35,10 +35,10 @@
 #define HMG_TRI_LO "ace_csw_m3CarryTripodLow"
 #define HMG_MAG "ace_csw_100Rnd_127x99_mag:3"
 //HAT
-#define HAT "ace_compat_rhs_usf3_tow_carry"
+#define HAT "CUP_TOW_carry"
 #define HAT_TRI_HI "ace_csw_m220CarryTripod"
 #define HAT_TRI_LO ""
-#define HAT_MAG "ace_compat_rhs_usf3_mag_ITOW"
+#define HAT_MAG "CUP_compats_TOW_M"
 // SAM
 #define SAM "launch_B_Titan_F"
 #define SAM_MAG "Titan_AA:3"
