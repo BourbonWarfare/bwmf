00c99cd405f3f4f4d73e7030bae3df0388b66966
bar56 <barred@ymail.com>
2022-09-20 23:54:03 -0500

    Update cz_vz58_m95w.hpp (#166)
    Change to a mag that works in the AR, Fix classname for SMG ammo.

--- Loadouts/cz_vz58_m95w.hpp
+++ Loadouts/cz_vz58_m95w.hpp
@@ -18,7 +18,7 @@ description = "Czechoslovakia: vz.58 - M95 Woodland";
 #define CARBINE_MAG RIFLE_MAG
 // AR
 #define AR "potato_arifle_RPK"
-#define AR_MAG "potato_75Rnd_762x39mm_tracer:6"
+#define AR_MAG "potato_magazine_762x39_75rnd_tracer_Red:6"
 // AT
 #define AT "rhs_weap_rpg7"
 #define AT_MAG "rhs_rpg7_PG7VL_mag:1"
@@ -55,8 +55,8 @@ description = "Czechoslovakia: vz.58 - M95 Woodland";
 #define SPOTTER_MAG RIFLE_MAG
 #define SPOTTER_ATTACHMENTS "CUP_acc_ANPEQ_2","CUP_optic_RCO"
 // SMG
-#define SMG "rhs_weap_savz61"
-#define SMG_MAG "rhsgref_20rnd_765x17_vz61:6"
+#define SMG "CUP_smg_SA61"
+#define SMG_MAG "CUP_20Rnd_B_765x17_Ball_M:8"
 // Pistol
 #define PISTOL "CUP_hgun_Duty"
 #define PISTOL_MAG "16Rnd_9x21_Mag:3"
