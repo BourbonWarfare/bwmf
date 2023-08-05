4980060cf7daed7f244d7ca325416cb70243deb6
Dan Thomas [BadWolf] <5899996+DanThomas64@users.noreply.github.com>
2023-08-04 23:51:37 -0300

    update inspect setting descriptors

--- scripts/inspect/settings.hpp
+++ scripts/inspect/settings.hpp
@@ -1,16 +1,20 @@
 #define SCRIPT inspect
 #include "..\common.hpp"
-#define INSPECT_TIME_MAX 10
-#define INSPECT_TIME_MIN 1
-#define INSPECT_FROM_VIC FALSE
-#define INSPECT_VIC "B_Heli_Light_01_armed_F"
-#define INSPECT_OBJECT "Land_Laptop_unfolded_F"
-#define INSPECT_SIDE west
-#define INSPECT_UNIT "B_Soldier_F"
-#define INSPECT_OBJECT_OFFSET [0,0,0]
-#define INSPECT_AREA TRUE
-#define INSPECT_RADIUS 10
-#define TRIGGER_RADIUS 30
+
+// SETTINGS
+#define INSPECT_TIME_MAX 10                       // Max inspect time in seconds
+#define INSPECT_TIME_MIN 1                        // Min inspect time in seconds
+#define INSPECT_FROM_VIC FALSE                    // Insepect from a vehicle?
+#define INSPECT_VIC "B_Heli_Light_01_armed_F"     // Vehicle to inspect from
+#define INSPECT_OBJECT "Land_Laptop_unfolded_F"   // Object to inspect
+#define INSPECT_SIDE west                         // Inspecting side
+#define INSPECT_UNIT "B_Soldier_F"                // Inspector class
+#define INSPECT_OBJECT_OFFSET [0,0,0]             // Inspect offset from center of object
+#define INSPECT_AREA TRUE                         // Multiple objects in given area?
+#define INSPECT_RADIUS 10                         // Required inspect radius
+#define TRIGGER_RADIUS 30                         // Radius at which inspect is available
+
+// VARIABLES
 SET_MISSION_VAR(time_max, INSPECT_TIME_MAX);
 SET_MISSION_VAR(time_min, INSPECT_TIME_MIN);
 SET_MISSION_VAR(vic, INSPECT_VIC);
