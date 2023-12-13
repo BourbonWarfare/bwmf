f6e9740c504c0876d67282b4abaa44dc96d3dc5a
Bailey Danyluk <baileydanyluk@gmail.com>
2023-12-12 21:08:05 -0700

    add supply crate loadout example

--- CfgLoadouts.hpp
+++ CfgLoadouts.hpp
@@ -13,6 +13,19 @@ class CfgLoadouts {
   // (1 will run normaly, 0 will leave them to vanilla defaults, -1 will clear and leave empty)
   setVehicleLoadouts = -1;
 
+  // Do Supply Box Loadouts
+  // (1 will run normaly, 0 will leave them to vanilla defaults, -1 will clear and leave empty)
+  setSupplyBoxLoadouts = 0;
+  class SupplyBoxes {
+    /* Example:
+      class B_supplyCrate_F {
+        TransportWeapons[] = {"arifle_TRG20_F"};
+        TransportMagazines[] = {"30Rnd_556x45_Stanag_red:8","30Rnd_556x45_Stanag_Tracer_Red:2"};
+        TransportItems[] = {"ACRE_PRC148:10"};
+      };
+    */
+  };
+
   // Fallback: use a basic soldiers loadout when the unit's classname isn't found (for Alive spawning random units)
   useFallback = 1;
 
