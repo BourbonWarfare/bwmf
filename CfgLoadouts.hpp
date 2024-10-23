cd8caadc9c7d2f789f5ed2e4daf10326adb0c5fa
lambdatiger <spam.stnuocca.sl@gmail.com>
2024-10-22 21:57:35 -0500

    Added more description for new supply box and vehicle loadout systems

--- CfgLoadouts.hpp
+++ CfgLoadouts.hpp
@@ -9,20 +9,46 @@ class CfgLoadouts {
   // Allow changeable optics on a global level, note: optic options obey allowMagnifiedOptics rules
   allowChangeableOptics = 1;
 
-  // Do Vehicle Loadouts
-  // (1 will run normaly, 0 will leave them to vanilla defaults, -1 will clear and leave empty)
+  // Vehicle Loadouts -- see "us_mx_mtp.hpp" for examples
+  // -1. Clear and leave inventories empty empty
+  //  0. Leave them to vanilla defaults
+  //  1. Defined supplies are put into vehicle's inventory
+  //  2. Store supplies from vehicle inventory into ammo boxes in vehicle cargo
+  //  3. Use ammo box definitions to create ammo boxes in vehicle's ACE cargo and clear inventory
   setVehicleLoadouts = -1;
 
   // Do Supply Box Loadouts
-  // (1 will run normaly, 0 will leave them to vanilla defaults, -1 will clear and leave empty)
+  // -1. Clear and leave inventories empty empty
+  //  0. Leave them to vanilla defaults
+  //  1. Defined supplies are put into box's inventory
+  //  2. Use ammo box definitions to create ammo boxes in box's ACE cargo and clear inventory
   setSupplyBoxLoadouts = 0;
   class SupplyBoxes {
-    /* Example:
-      class B_supplyCrate_F {
-        TransportWeapons[] = {"arifle_TRG20_F"};
+    /* Example: setSupplyBoxLoadouts = 1
+    class B_supplyCrate_F {
+      TransportWeapons[] = {"arifle_TRG20_F"};
+      TransportMagazines[] = {"30Rnd_556x45_Stanag_red:8","30Rnd_556x45_Stanag_Tracer_Red:2"};
+      TransportItems[] = {"ACRE_PRC148:10"};
+    };
+    */
+    /* Example: setSupplyBoxLoadouts = 2
+    class I_supplyCrate_F { // overarching box
+      addMarkingActions = 0; // 0 for none, 1 for single use of smoke/flare
+      boxCustomName = "Easy To Find Name";
+      boxSpace = 8; // ACE cargo space box will have
+      class Box_NATO_Ammo_F { // sub box
+        boxCount = 4; // number of boxes to create
+        boxCustomName = "Resupply Box";
+        TransportWeapons[] = {"arifle_TRG20_F:4"};
         TransportMagazines[] = {"30Rnd_556x45_Stanag_red:8","30Rnd_556x45_Stanag_Tracer_Red:2"};
-        TransportItems[] = {"ACRE_PRC148:10"};
+        TransportItems[] = {"ACE_elasticBandage:25","ACE_packingBandage:15","ACE_tourniquet:5","ACE_splint:5"};
       };
+    };
+    class B_supplyCrate_F { // falls back to work the same way as Mode 1 without defined sub-boxes
+      TransportWeapons[] = {"arifle_TRG20_F"};
+      TransportMagazines[] = {"30Rnd_556x45_Stanag_red:8","30Rnd_556x45_Stanag_Tracer_Red:2"};
+      TransportItems[] = {"ACRE_PRC148:10"};
+    };
     */
   };
 
@@ -38,9 +64,9 @@ class CfgLoadouts {
 
   // cleanup.bat will delete any loadouts not in active use (so make sure to include before running)
 
-  // Blufor/Indfor/Opfor folders are just for organization, they can be used with any faction 
+  // Blufor/Indfor/Opfor folders are just for organization, they can be used with any faction
   // e.g. Blufor\us_mx_mtp.hpp for potato_e is fine
-  
+
   // Loadouts can be either kept in the Loadouts\Blufor\ folders or moved up to Loadouts\
 
   // West factions
